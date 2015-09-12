bf67fd5895e32b03cf98ae37a4d2177a7964f110
IncoCode <akulich.dimon@gmail.com>
2015-09-12 22:05:24 +0300

    Move InteriorController in Controller folder

---
+++ Code/BuyApartments/Controller/InteriorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using BuyApartments.Model;
+using BuyApartments.Model.JSON;
+using GTA.Math;
+using Newtonsoft.Json;
+
+namespace BuyApartments.Controller
+{
+    internal class InteriorController
+    {
+        private readonly List<Interior> _interiors;
+
+        public InteriorController()
+        {
+            this._interiors = new List<Interior>();
+
+            InteriorsListJSON parsedInteriors = JsonConvert.DeserializeObject<InteriorsListJSON>( Properties.Resource.Interiors );
+            foreach ( InteriorJSON interiorJSON in parsedInteriors.Interiors )
+            {
+                var interior = new Interior( interiorJSON.Coordinates, interiorJSON.Name );
+            }
+        }
+    }
+}
