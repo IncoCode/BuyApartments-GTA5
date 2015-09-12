ad22c21c5b03159936ffe08156e4afa57f72f003
IncoCode <akulich.dimon@gmail.com>
2015-09-12 21:39:18 +0300

    Add Models and iFruitAddon

--- Code/BuyApartments/BuyApartments.cs
+++ Code/BuyApartments/BuyApartments.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GTA;
+using iFruitAddon;
+using Ini;
 
 namespace BuyApartments
 {
-    public class BuyApartments
+    public class BuyApartments : Script
     {
+        private readonly IniFile _settings;
+        private readonly CustomiFruit _fruit;
+
+        public BuyApartments()
+        {
+            this.Tick += this.BuyApartments_Tick;
+
+            this._fruit = new CustomiFruit();
+            var contact = new iFruitContact( "Apartment Dealer", this._fruit.Contacts.Count );
+            contact.Selected += ( sender, args ) => UI.ShowSubtitle( "1" );
+            this._fruit.Contacts.Add( contact );
+        }
+
+        private void BuyApartments_Tick( object sender, EventArgs e )
+        {
+            this._fruit.Update();
+        }
     }
 }
---
+++ Code/BuyApartments/Model/Houses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyApartments.Model
+{
+    public class Location
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+    }
+
+    public class House
+    {
+        public string Name { get; set; }
+        public string Interior { get; set; }
+        public int Price { get; set; }
+        public Location Location { get; set; }
+    }
+
+    public class HousesList
+    {
+        public List<House> Houses { get; set; }
+    }
+}
